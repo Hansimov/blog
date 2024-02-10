eb728ce4d04ffacb1e4883efcd0f921e7d8a7599
Hansimov <591172499@qq.com>
2024-02-11 04:26:27 +0800

    :gem: [Feature] Enable comments with giscus in Layout slot

---
+++ docs/.vitepress/theme/components/Comments.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useData } from "vitepress";
+
+const { frontmatter, title } = useData();
+</script>
+
+<template>
+    <div v-if="frontmatter.comments !== false" :key="title" class="giscus">
+        <component
+            :is="'script'"
+            src="https://giscus.app/client.js"
+            data-repo="Hansimov/blog"
+            data-repo-id="R_kgDOLQOGCA"
+            data-category="Blog Comments"
+            data-category-id="DIC_kwDOLQOGCM4CdHfm"
+            data-mapping="pathname"
+            data-strict="0"
+            data-reactions-enabled="0"
+            data-emit-metadata="0"
+            data-input-position="bottom"
+            data-theme="light"
+            data-lang="en"
+            crossorigin="anonymous"
+            async
+        />
+    </div>
+</template>
---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,7 @@
+import DefaultTheme from 'vitepress/theme'
+import LayoutWithComments from './LayoutWithComments.vue'
+
+export default {
+    extends: DefaultTheme,
+    Layout: LayoutWithComments
+}
